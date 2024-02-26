fb09502ada20a9ec31352684e3a5d85eb0e63718
IraVovk96 <148684180+IraVovk96@users.noreply.github.com>
2024-02-26 18:45:04 +0100

    new change

--- js/task-1.js
+++ js/task-1.js
@@ -1,13 +1,11 @@
-const reultOfItems = document.getElementsByClassName("item");
-console.log(`Number of categories: ${reultOfItems.length}`);
+const resultOfItems = Array.from (document.getElementsByClassName("item"));
+console.log(`Number of categories: ${resultOfItems.length}`);
 
-const titles = document.querySelectorAll("h2");
-console.log(titles);
-titles.forEach(function (title){
-    const titleName = title.textContent;
-    console.log(`Category: ${titleName}`);
-    const categoriesName = document.querySelectorAll("ul>li");
-    console.log(`Elements: ${categoriesName.length}`);
+resultOfItems.forEach(function (item){
+    const title = item.querySelector("h2").textContent;
+    console.log(`Category:${title}`);
+    const categoriesName = item.querySelectorAll("ul > li");
+    console.log(`Elements:${categoriesName.length}`);
 })
 
 
